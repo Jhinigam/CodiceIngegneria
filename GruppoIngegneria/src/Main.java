7254ef9eb4cd4a953cd9b38b13f472d1c6d0cd65
Jhinigam <marcoagostinelli95@gmail.com>
2024-01-18 12:05:10 +0100

    Modificato il file "Main" aggiungendo il database online condiviso

--- GruppoIngegneria/src/Main.java
+++ GruppoIngegneria/src/Main.java
@@ -7,9 +7,9 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
-        final String DB_URL = "jdbc:mysql://localhost:3306/sys?useSSL=false";
-        final String USER = "Marco";
-        final String PASS = "admin";
+        final String DB_URL = "jdbc:mysql://sql11.freesqldatabase.com:3306/sql11677848";
+        final String USER = "sql11677848";
+        final String PASS = "BQ9aLwIJDb";
         final String QUERY = "SELECT * FROM comune";
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
