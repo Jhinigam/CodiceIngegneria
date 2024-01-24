fa0382465aeb94f5cbc2caadb785ad3ee3bef4fc
Jhinigam <marcoagostinelli95@gmail.com>
2024-01-24 17:09:30 +0100

    Provando a fixare il driver Jbdc

--- src/main/java/Blarb/ProgettoIngegneria/Controller/AbstractController.java
+++ src/main/java/Blarb/ProgettoIngegneria/Controller/AbstractController.java
@@ -3,7 +3,6 @@ package Blarb.ProgettoIngegneria.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import org.springframework.stereotype.Component;
 import javax.swing.text.html.parser.Entity;
 
