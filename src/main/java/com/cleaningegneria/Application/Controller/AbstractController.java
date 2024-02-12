fd5f310c52a4440deddd26c64fb2fd2e21f64dd3
Kama-Verga <132174425+Kama-Verga@users.noreply.github.com>
2024-02-12 18:24:04 +0100

    creazione utente, in HTML e JS, e relativi controller, (non ancora funzionante)

--- src/main/java/com/cleaningegneria/Application/Controller/AbstractController.java
+++ src/main/java/com/cleaningegneria/Application/Controller/AbstractController.java
@@ -2,6 +2,7 @@ package com.cleaningegneria.Application.Controller;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
@@ -13,9 +14,17 @@ public abstract class AbstractController {
         return "OK";
     }
 
+    @GetMapping("/Status")
+    @ResponseBody
+    public String statusPersonalizzato(@RequestParam String P){
+        return "OK " + P;
+    }
+
     @GetMapping("/createPost")
     @ResponseBody
     public String creazionePost(){
         return "Post creato";
     }
+
+
 }
