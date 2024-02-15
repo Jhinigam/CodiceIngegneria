6e345e71da9209d4513215ab730f35b617d0b9fa
Jhinigam <marcoagostinelli95@gmail.com>
2024-02-15 13:21:12 +0100

    Aggiunta funzionalità Utente Service

--- src/main/java/com/cleaningegneria/Application/Controller/UtenteController.java
+++ src/main/java/com/cleaningegneria/Application/Controller/UtenteController.java
@@ -1,21 +1,20 @@
 package com.cleaningegneria.Application.Controller;
 
 import com.cleaningegneria.Application.Models.DTO.ModificaRuoloDTO;
-import com.cleaningegneria.Application.Models.DTO.UtenteDTO;
-import com.cleaningegneria.Application.Models.Entity.Ruolo;
+import com.cleaningegneria.Application.Models.DTO.CreazioneUtenteDTO;
 import com.cleaningegneria.Application.Models.Entity.Utente;
-import com.cleaningegneria.Application.Repository.UtenteRepository;
+import com.cleaningegneria.Application.Service.UtenteService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Utente")
 public class UtenteController extends AbstractController {
 
+    private UtenteService utenteService;
     private Utente User;
-    private final UtenteRepository utenteRepository;
+    public UtenteController() {
+        utenteService = new UtenteService();
 
-    public UtenteController(UtenteRepository utenteRepository) {
-        this.utenteRepository = utenteRepository;
     }
 
 
@@ -23,23 +22,16 @@ public class UtenteController extends AbstractController {
     @PutMapping("/MRuolo")
     @ResponseBody
     public String CambioRuolo(@RequestBody ModificaRuoloDTO uDTO){
-        User.setRuolo(StringToRuolo(uDTO.getNewRuolo()));
+        //User.setRuolo(StringToRuolo(uDTO.getNewRuolo()));
         return "ok";
     }
 
     @CrossOrigin(origins = "http://localhost:63342/")
     @PutMapping("/Creazione")
     @ResponseBody
-    public String CreateUtente(@RequestBody UtenteDTO uDTO){
+    public Utente CreateUtente(@RequestBody CreazioneUtenteDTO uDTO){
+        return utenteService.creaUtente(uDTO.getNome(),uDTO.getCognome(), uDTO.getEmail(), uDTO.getEta(), uDTO.getRuolo(), uDTO.getComune());
 
-        User = new Utente(uDTO.getNome(),
-                uDTO.getCognome(),
-                uDTO.getEmail(),
-                uDTO.getEta(),
-                StringToRuolo(uDTO.getRuolo()),
-                uDTO.getComune());
-
-        return "ok";
     }
 
 
@@ -52,34 +44,5 @@ public class UtenteController extends AbstractController {
     }
 
 
-    public Ruolo IntToRuolo(int A){
-        switch (A){
-            case 0:
-                return Ruolo.Contributore;
-            case 1:
-                return Ruolo.Turista;
-            case 2:
-                return Ruolo.ContributoreAutorizzato;
-            case 3:
-                return Ruolo.TuristaAutorizzato;
-            case 4:
-                return Ruolo.Curatore;
-        }
-        return Ruolo.Animatore;
-    }
-    public Ruolo StringToRuolo(String A){
-        switch (A){
-            case "Contributore":
-                return Ruolo.Contributore;
-            case "Turista":
-                return Ruolo.Turista;
-            case "ContributoreAutorizzato":
-                return Ruolo.ContributoreAutorizzato;
-            case "TuristaAutorizzato":
-                return Ruolo.TuristaAutorizzato;
-            case "Curatore":
-                return Ruolo.Curatore;
-        }
-        return Ruolo.Animatore;
-    }
+
 }
--- src/main/java/com/cleaningegneria/Application/Service/UtenteService.java
+++ src/main/java/com/cleaningegneria/Application/Service/UtenteService.java
@@ -1,4 +1,49 @@
 package com.cleaningegneria.Application.Service;
 
+import com.cleaningegneria.Application.Models.Entity.Ruolo;
+import com.cleaningegneria.Application.Models.Entity.Utente;
+import com.cleaningegneria.Application.Repository.UtenteRepository;
+
 public class UtenteService {
+    private UtenteRepository utenteRepository;
+    private Utente User;
+
+    public UtenteService(){
+
+    }
+
+    public Utente creaUtente(String nome, String cognome, String email, int eta, String ruolo, String comune){
+        User = new Utente(nome,cognome,email,eta,StringToRuolo(ruolo),comune);
+        return User;
+    }
+    public Ruolo IntToRuolo(int A){
+        switch (A){
+            case 0:
+                return Ruolo.Contributore;
+            case 1:
+                return Ruolo.Turista;
+            case 2:
+                return Ruolo.ContributoreAutorizzato;
+            case 3:
+                return Ruolo.TuristaAutorizzato;
+            case 4:
+                return Ruolo.Curatore;
+        }
+        return Ruolo.Animatore;
+    }
+    public Ruolo StringToRuolo(String A){
+        switch (A){
+            case "Contributore":
+                return Ruolo.Contributore;
+            case "Turista":
+                return Ruolo.Turista;
+            case "ContributoreAutorizzato":
+                return Ruolo.ContributoreAutorizzato;
+            case "TuristaAutorizzato":
+                return Ruolo.TuristaAutorizzato;
+            case "Curatore":
+                return Ruolo.Curatore;
+        }
+        return Ruolo.Animatore;
+    }
 }
