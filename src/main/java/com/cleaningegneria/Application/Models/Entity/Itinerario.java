80b799f3faba52827ed03ab1373da3407009846b
Jhinigam <marcoagostinelli95@gmail.com>
2024-02-19 19:43:09 +0100

    Create tabelle delle chiavi esterne Post-Itinerario e Evento-Partecipanti

--- src/main/java/com/cleaningegneria/Application/Models/Entity/Itinerario.java
+++ src/main/java/com/cleaningegneria/Application/Models/Entity/Itinerario.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,14 +21,16 @@ public class Itinerario {
     private Integer id;
     private Timestamp datapubblicazione;
     @ManyToOne
-    private Utente utente;
+    private Utente creatore;
+    @ManyToMany
+    private List<Post> contenuti;
     private String descrizione;
     private boolean pending;
 
     public Itinerario(Timestamp datapubblicazione, String comune, Integer idutente, String descrizione, boolean pending) {
         this.datapubblicazione = datapubblicazione;
-        this.utente = new Utente();
-        this.utente.setId(idutente);
+        this.creatore = new Utente();
+        this.creatore.setId(idutente);
         this.descrizione = descrizione;
         this.pending = pending;
     }
